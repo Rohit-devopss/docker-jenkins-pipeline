8574b1fc7c3020ec4af9b0636bd067a329d7b92f
Sharad Durgawad <sdurgawad@avaya.com>
2020-07-13 21:33:53 +0530

    Added hello-world sample java prpject and Jenkins Dockerfile

---
+++ hello-world/src/main/java/helloworld/helloworld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		 System.out.println( "Hello, This is Sharad Durgawad, a DevOps, Cloud and Automation enthusiast !!!" );
+	}
+
+}
